fafe0dba947912bf21e6e81d760ae478de4a609d
andrew <inter>
2023-06-14 20:31:33 +0000

    first commit

---
+++ mpttune/model/__init__.py
@@ -0,0 +1,28 @@
+from .mpt.config import (
+    MPT7B8bitConfig, MPT7BChat8bitConfig, MPT7BInstruct8bitConfig, MPT7BStorywriter4BitConfig, MPT7BStorywriter8bitConfig)
+
+
+MODEL_CONFIGS = {
+    MPT7B8bitConfig.name: MPT7B8bitConfig,
+    MPT7BChat8bitConfig.name: MPT7BChat8bitConfig,
+    MPT7BInstruct8bitConfig.name: MPT7BInstruct8bitConfig,
+    MPT7BStorywriter8bitConfig.name: MPT7BStorywriter8bitConfig,
+    MPT7BStorywriter4BitConfig.name: MPT7BStorywriter4BitConfig,
+}
+
+
+def load_model(model_name: str, weights, half=False, backend='triton'):
+    if model_name not in MODEL_CONFIGS:
+        raise ValueError(f"Invalid model name: {model_name}")
+
+    model_config = MODEL_CONFIGS[model_name]
+
+    if model_name in MODEL_CONFIGS:
+        from .mpt.model import load_model
+        model, tokenizer = load_model(model_config, weights, half=half, backend=backend)
+
+    else:
+        raise ValueError(f"Invalid model name: {model_name}")
+
+    model.eval()
+    return model, tokenizer
---
+++ mpttune/model/mpt/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,53 @@
+class MPT7B8bitConfig:
+    name = 'mpt-7b'
+    hf_config_name = "mosaicml/mpt-7b"
+    hf_tokenizer_config = "EleutherAI/gpt-neox-20b"
+    bits = 16
+    groupsize = None
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class MPT7BInstruct8bitConfig:
+    name = 'mpt-7b-instruct'
+    hf_config_name = "mosaicml/mpt-7b-instruct"
+    hf_tokenizer_config = "EleutherAI/gpt-neox-20b"
+    bits = 8
+    groupsize = None
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class MPT7BChat8bitConfig:
+    name = 'mpt-7b-chat'
+    hf_config_name = "mosaicml/mpt-7b-chat"
+    hf_tokenizer_config = "EleutherAI/gpt-neox-20b"
+    bits = 8
+    groupsize = None
+    max_seq_len = 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class MPT7BStorywriter8bitConfig:
+    name = 'mpt-7b-storywriter'
+    hf_config_name = "mosaicml/mpt-7b-storywriter"
+    hf_tokenizer_config = "EleutherAI/gpt-neox-20b"
+    bits = 8
+    groupsize = None
+    max_seq_len = 5 * 2048
+    attn_impl = 'torch'
+    device_map = "auto"
+
+
+class MPT7BStorywriter4BitConfig:
+    name = 'mpt-7b-storywriter-4bit'
+    hf_config_name = "OccamRazor/mpt-7b-storywriter-4bit-128g"
+    hf_tokenizer_config = "EleutherAI/gpt-neox-20b"
+    bits = 4
+    groupsize = 128
+    max_seq_len = 5 * 2048
+    attn_impl = 'torch'
+    device_map = "auto"
